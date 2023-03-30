3172d8a7579736ac971ef3f083906ae43eb44a48
farida fadilah <faridafadilah42807@gmail.com>
2023-03-30 13:40:27 +0700

    app wisata bandung with data dummy

---
+++ myapp/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+// import 'package:myapp/detail_screen.dart';
+import 'package:myapp/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      // home: const DetailScreen(),
+      home: const MainScreen(),
+    );
+  }
+}
